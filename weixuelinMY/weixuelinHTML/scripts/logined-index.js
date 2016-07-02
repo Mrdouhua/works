7fe97c5bb58eaa3230cdff66fc4d42967cc318be
Mrdouhua <1377392896@qq.com>
2016-07-02 13:43:53 +0800

    我的部分

---
+++ weixuelinMY/weixuelinHTML/scripts/logined-index.js
@@ -0,0 +1,233 @@
+window.onload = function(){
+        scroll();	
+        nav();
+       
+
+}
+/*滑动的导航栏菜单*/
+function nav(){
+    var oNav = document.getElementById('nav');
+    var oLi = oNav.getElementsByTagName('li');
+    var oCur = oLi[oLi.length-1];
+    var i = 0;
+
+    for(i=0;i<oLi.length-1;i++){
+        oLi[i].onmouseover = function(){
+            startMove(oCur,this.offsetLeft);
+        };
+        oLi[i].onmouseout = function(){
+            startMove(oCur,0);
+        };
+    }
+    var iSpeed = 0;
+    var left = 0;
+    function startMove(obj,iTarget){
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function(){
+            iSpeed = (iTarget - obj.offsetLeft)/5;
+            iSpeed *= 0.5;
+            left += iSpeed;
+
+            if(Math.abs(iSpeed)<1 && Math.abs(left-iTarget)<1){
+                clearInterval(obj.timer);
+                obj.style.left = iTarget + 'px';
+            } else
+            {
+                obj.style.left = left + 'px';
+            }
+        },30)
+    }
+}
+/*图片轮播*/
+function scroll(){   
+    var container = document.getElementById('container');
+    var list = document.getElementById('list');    
+    var buttons = document.getElementById('buttons').getElementsByTagName('span');
+    var prev = document.getElementById('prev');
+    var next = document.getElementById('next');
+    var index = 1;
+    var len = 5;
+    var animated = false;
+    var interval = 6000;
+    var timer;
+
+    function animate (offset) {
+        if (offset == 0) {
+            return;
+        }
+        animated = true;
+        var time = 300;
+        var inteval = 10;
+        var speed = offset/(time/inteval);
+        var left = parseInt(list.style.left) + offset;
+
+        var go = function (){
+            if ( (speed > 0 && parseInt(list.style.left) < left) || (speed < 0 && parseInt(list.style.left) > left)) {
+                list.style.left = parseInt(list.style.left) + speed + 'px';
+                setTimeout(go, inteval);
+            }
+            else {
+                list.style.left = left + 'px';
+            if(left>-200){
+                list.style.left = -1348 * len + 'px';
+            }
+            if(left<(-1348 * len)) {
+                list.style.left = '-1348px';
+            }
+                animated = false;
+            }
+        }
+        go();
+    }
+
+    function showButton() {
+        for (var i = 0; i < buttons.length ; i++) {
+            if( buttons[i].className == 'on'){
+                buttons[i].className = '';
+                break;
+            }
+        }
+        buttons[index - 1].className = 'on';
+    }
+
+    function play() {
+        timer = setTimeout(function () {
+            next.onclick();
+            play();
+        }, interval);
+    }
+    function stop() {
+        clearTimeout(timer);
+    }
+
+    next.onclick = function () {
+        if (animated) {
+            return;
+        }
+        if (index == 5) {
+            index = 1;
+        }
+        else {
+            index += 1;
+        }
+        animate(-1348);
+        showButton();
+    }
+    prev.onclick = function () {
+        if (animated) {
+            return;
+        }
+        if (index == 1) {
+            index = 5;
+        }
+        else {
+            index -= 1;
+        }
+        animate(1348);
+        showButton();
+    }
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function () {
+            if (animated) {
+                return;
+            }
+            if(this.className == 'on') {
+                return;
+            }
+            var myIndex = parseInt(this.getAttribute('index'));
+            var offset = -1348 * (myIndex - index);
+
+            animate(offset);
+            index = myIndex;
+            showButton();
+        }
+    }
+
+    container.onmouseover = stop;
+    container.onmouseout = play;
+
+    play();            
+}
+/*弹出层*/       
+function openNew(){
+    //获取页面的高度和宽度
+    var sWidth=document.body.scrollWidth;
+    var sHeight=document.body.scrollHeight;
+    
+    //获取页面的可视区域高度和宽度
+    var wHeight=document.documentElement.clientHeight;
+    
+    var oMask=document.createElement("div");
+        oMask.id="mask";
+        oMask.style.height=sHeight+"px";
+        oMask.style.width=sWidth+"px";
+        document.body.appendChild(oMask);
+    /*var oLogin=document.createElement("div");
+        oLogin.id="login";
+        oLogin.innerHTML="<div class='loginCon'><div id='close'>点击关闭</div><form method='post' action=''><label>用户名:</label><input type='text' id='username' class='username' required /><br/><label>&nbsp;密码:</label><input type='password' id='password' class='password' required /><br/><button type='submit' id='submit' class='submit' onclick='fun()'>登录</button><button type='reset' id='reset' class='reset'>重置</button></form></div>";
+        document.body.appendChild(oLogin);*/
+    var oLogin = document.getElementById('login');
+        oLogin.style.display = "block";
+
+    //获取登陆框的宽和高
+    var dHeight=oLogin.offsetHeight;
+    var dWidth=oLogin.offsetWidth;
+        //设置登陆框的left和top
+        oLogin.style.left=sWidth/2-dWidth/2+"px";
+        oLogin.style.top=wHeight/2-dHeight/2+"px";
+    //点击关闭按钮
+    var oClose=document.getElementById("close");
+    
+        //点击登陆框以外的区域也可以关闭登陆框
+        oClose.onclick=oMask.onclick=function(){
+                    /*document.body.removeChild(oLogin);
+                    document.body.removeChild(oMask);*/
+                    oMask.style.display = "none";
+                    oLogin.style.display = "none";
+                    };
+};
+/*表单提交*/
+$(function(){
+    $('#submit').click(function(){
+        var password = document.getElementById('password').value;
+        var username = document.getElementById('username').value;
+        var patt1 = new RegExp("^[A-Za-z0-9]+$");
+        var patt2 = new RegExp("^[0-9a-zA-Z]{6,20}$");
+        if(!patt2.test(username)){
+            alert("用户名长度在6-20之间，由字母和数字组成！");
+        }
+        if(password.length<6||password.length>16){
+            alert("密码必须为6到16位");
+            return false;
+        }
+        if(!patt1.test(password)){
+            alert("只能为数字和英文字母");
+            return false;
+        }
+        var formData = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        };
+        var subData = $.param(formData,true);
+        var url = '_self?'+filed.id+"="+f_value+"&timestamp"+new Date().getTime();
+        $.post('url',subData,function(data,textStatus){
+            if(data !='1'){
+                window.location.href = data;
+            } else {
+                alert('登录失败');
+                return false;
+            }
+        });
+        return false;  
+    })
+        
+})
+$(function(){
+    $('#navbar-user').click(function(){
+        $('#avatar').css('display','block');
+    });
+    $('#content').click(function(){
+        $('#avatar').css('display','none');
+     })
+})
---
+++ weixuelinMY/weixuelinHTML/scripts/personal-center.js
@@ -0,0 +1,192 @@
+window.onload = function(){
+    nav();
+    tabScroll();
+}
+
+/*滑动的导航栏菜单开始*/
+function nav(){
+    var oNav = document.getElementById('nav');
+        var oLi = oNav.getElementsByTagName('li');
+        var oCur = oLi[oLi.length-1];
+        var i = 0;
+
+         for(i=0;i<oLi.length-1;i++){
+            oLi[i].onmouseover = function(){
+                startMove(oCur,this.offsetLeft);
+            };
+            oLi[i].onmouseout = function(){
+                startMove(oCur,520);
+            };
+        }
+    var iSpeed = 0;
+    var left = 0;
+    function startMove(obj,iTarget){
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function(){
+            iSpeed = (iTarget - obj.offsetLeft)/5;
+            iSpeed *= 0.5;
+            left += iSpeed;
+
+            if(Math.abs(iSpeed)<1 && Math.abs(left-iTarget)<1){
+                clearInterval(obj.timer);
+                obj.style.left = iTarget + 'px';
+            } else
+            {
+                obj.style.left = left + 'px';
+            }
+        },30)
+    }
+}
+/*头像部分*/
+$(function(){
+    $('#navbar-user').click(function(){
+        $('#avatar').css('display','block');
+    });
+    $('#content').click(function(){
+        $('#avatar').css('display','none');
+     })
+    var $oli = $('ul.tab-titles li');
+    $oli.click(function(){
+        $(this).addClass('selected')
+        .siblings().removeClass('selected');
+        var index = $oli.index(this);
+        $('div.tab_box>div')
+            .eq(index).show()
+            .siblings().hide();
+    });
+})
+$(function(){
+    //昵称的验证
+    $("#nickName").blur(function(){
+       //判断是否为空或者全为空格
+        if($('#nickName').val().length == 0 || /^\s+$/g.test($(this).val())){
+            $(".nickAfter").css("visibility","visible").empty().append("昵称不能为空或者全为空格！");
+        } else {
+            $(".nickAfter").css("visibility","visible").empty().append("昵称输入正确！");
+        }
+    });
+    $("#password").blur(function(){
+       //判断是否为空或者全为空格
+        if(!/^[A-Za-z0-9]{6,12}$/.test($(this).val())){
+            $(".passwordAfter").css("visibility","visible").empty().append("请输入6-12位数字或字母！");
+        } else {
+            $(".passwordAfter").css("visibility","visible").empty().append("输入正确！");
+        }
+    });
+    $("#school").blur(function(){
+       //判断学校是否为中文
+        if($(this).val() === "" || !/^[\u4e00-\u9fa5]{0,}$/.test($(this).val())){
+            $(".schoolAfter").css("visibility","visible").empty().append("请输入中文！");
+        } else {
+            $(".schoolAfter").css("visibility","visible").empty().append("输入正确！");
+        }
+    });
+})
+$('#inforSend').click(function(){
+    var inforData = {
+        nickName: $('#nickName').val(),
+        password: $('#password').val(),
+        sex: $("input[name='gender']:checked").val(),
+        school: $('school').val(),
+        area: $('area').val(),
+        introduction: $('introduction').val()
+    };
+    var sendData = $.param(inforData,true);
+    var url = "servlet/User_Upgrade?"+$(this).attr("name")+"="+$(this).val()+"&timestamp"+new Date().getTime();
+    ajax.post(url,sendData,function(data,textStatus){
+        var testData = jquery.parseJSON(data);
+        if (testData=='true') {
+            alert('修改成功！');
+            window.location="personal-center.jsp";
+        } else {
+            alert('修改失败');
+            return false;
+        }
+    })
+})
+//学习计划分页功能
+$(function(){
+    (function(){
+        var $myPlan = $(".myPlan");
+        var itemLength = $myPlan.length;
+        var pageNum = Math.ceil(itemLength/3);
+        var page = 1;
+
+        for(var i=1;i <= pageNum;i++){
+            (function(i){
+                var li = "<li class='myPlanPageItem'>"+i+"</li>";
+                $(".myPlanPage").append(li);
+
+            })(i);
+        }
+        (function(){
+            $(".myPlan").css('display','none');
+            for(var i=0;i<3;i++){
+                (function(i){
+                    if($(".myPlan")[i]){
+                        $(".myPlan")[i].style.display='block';
+                    }
+                })(i);
+            }
+        })();
+
+        $(".myPlanTabBef").click(function(){
+
+            // 判断当前页面状态
+            if(page !== 1) {
+                // 清除当前页数课程
+                $(".myPlan").css('display','none');
+                // 缓存当前页数状态
+                page = page - 1;
+                // 渲染上一页课程
+                for(var i = (page-1)*3; i < page*3; i++) {
+                    (function(i){
+                        if( $(".myPlan")[i] ) {
+                            $(".myPlan")[i].style.display = 'block';
+                        }
+                    })(i);
+                }
+            }
+        });
+        // 下一页跳转
+        $(".myPlanTabNex").click(function(){
+
+            // 判断当前页面状态
+            if(page !== pageNum) {
+                // 清除当前页数课程
+                $(".myPlan").css('display','none');
+                // 缓存当前页数状态
+                page = page + 1;
+                // 渲染下一页课程
+                for(var i = (page-1)*3; i < page*3; i++) {
+                    (function(i){
+                        if( $(".myPlan")[i] ) {
+                            $(".myPlan")[i].style.display = 'block';
+                        }
+                    })(i);
+                }
+            }
+        });
+        // 分页跳转
+        $(".myPlanPageItem").click(function(){
+
+            // 清除当前页数课程
+            $(".myPlan").css('display','none');
+
+            // 判断和缓存当前页面状态
+            page = $(".myPlanPageItem").index(this) + 1;
+
+            // 渲染下一页课程
+            for(var i = (page-1)*3; i < page*3; i++) {
+                (function(i){
+                    if( $(".myPlan")[i] ) {
+                        $(".myPlan")[i].style.display = 'block';
+                    }
+                })(i);
+            }
+            
+        });
+    })();
+});
+    
+    
